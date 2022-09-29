74641c54806ddea2458da46d6afa9c244b40cf94
WangZhenghai <15839366985@163.com>
2022-09-29 18:36:54 +0800

    修复：docker 登录

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 docker image prune -f
+docker login -u docker-1664447769279 -p 40cd3a66efd32cae7b4f8ae8ac789ff3e0c934c8 seaes-docker.pkg.coding.net
 docker pull seaes-docker.pkg.coding.net/seashellw/docker/server:latest
 docker stop server || true
 docker rm server || true
