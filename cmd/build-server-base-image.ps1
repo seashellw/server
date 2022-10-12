bf9fc8821f0d460e2aacbc75497a7cf1e34ada1e
WangZhenghai <15839366985@163.com>
2022-10-12 14:00:42 +0800

    构建：将构建流程移动至云

--- cmd/build-server-base-image.ps1
+++
@@ -1,4 +0,0 @@
-docker build -t server-base -f ./cmd/Dockerfile .
-docker tag server-base seaes-docker.pkg.coding.net/seashellw/docker/server-base:latest
-docker push seaes-docker.pkg.coding.net/seashellw/docker/server-base:latest
-
