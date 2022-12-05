13c625ef18b76d32b44d16b06b098b662e99c53b
seashellw <15839366985@163.com>
2022-12-05 12:39:41 +0000

    修复：dev 容器

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,9 +1,4 @@
-apt update && apt upgrade -y && apt install zsh -y
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-chsh -s /bin/zsh
-
 npm install -g pnpm
-SHELL=bash pnpm setup
-source /root/.bashrc
+SHELL=bash pnpm setup && source /home/vscode/.bashrc
 pnpm install
 pnpm db:gen
