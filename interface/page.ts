a329bc5caa8686319159d82c60d78268a83990f5
WangZhenghai <15839366985@163.com>
2022-12-06 09:22:13 +0000

    新增：热搜榜分页

--- interface/page.ts
+++ interface/page.ts
@@ -6,13 +6,13 @@ export abstract class Page {
   pageSize: number = 0; // 每页的数量
   size?: number; // 当前页的数量
   total?: number; // 总记录数
-  pages?: number; // 总页数
+  totalPage?: number; // 总页数
 }
 
 export const setTotal = (page: Page, total: number) => {
   page.total = total;
-  page.pages = Math.ceil(total / page.pageSize);
-  page.current = Math.min(page.current, page.pages);
+  page.totalPage = Math.ceil(total / page.pageSize);
+  page.current = Math.min(page.current, page.totalPage);
   page.size = Math.min(
     page.pageSize,
     total - (page.current - 1) * page.pageSize
--- server/api/tools/top-search.ts
+++ server/api/tools/top-search.ts
@@ -1,43 +1,98 @@
-import { defineHandler } from "@/util/util";
+import { Page, setTotal } from "@/interface/page";
+import { defineHandler, useCacheFn } from "@/util/util";
 import destr from "destr";
-import { SE, useCacheFn } from "@/util/util";
-import { get } from "https";
+import { request } from "undici";
 
-const getTopSearch = async (url = "https://tenapi.cn/resou") => {
-  return new Promise<string>((resolve) => {
-    let text = "";
-    get(url, (res) => {
-      if (res.headers.location?.startsWith("https")) {
-        getTopSearch(res.headers.location).then((data) => resolve(data));
-        return;
-      }
-      res.on("data", (d) => {
-        text += d;
-      });
-      res.on("end", () => {
-        resolve(text);
-      });
-    });
-  });
+const fetchData = async (url: string) => {
+  let res = await request(url, {
+    maxRedirections: 10,
+  }).then((res) => res.body.text());
+  return res;
 };
 
-const fetchData = useCacheFn(async () => {
-  let res = "";
-  try {
-    res = await getTopSearch();
-  } catch (e) {
-    console.error(e);
-    throw new SE(500, `${e}`);
-  }
-  let list = destr(res)?.list;
-  if (!(list instanceof Array)) {
-    throw new SE(500, `${list}`);
-  }
-  return list;
+const getTopSearch = useCacheFn(async () => {
+  let res: {
+    title: string;
+    url: string;
+    type: string;
+  }[] = [];
+  await Promise.all([
+    (async () => {
+      let text = await fetchData("https://tenapi.cn/resou");
+      let list = destr(text)?.list;
+      if (list?.length) {
+        res.push(
+          ...list.map((item: any) => ({
+            title: item.name,
+            url: item.url,
+            type: "微博",
+          }))
+        );
+      }
+    })(),
+    (async () => {
+      let text = await fetchData("https://tenapi.cn/baiduhot");
+      let list = destr(text)?.list;
+      if (list?.length) {
+        res.push(
+          ...list.map((item: any) => ({
+            title: item.name,
+            url: item.url,
+            type: "百度",
+          }))
+        );
+      }
+    })(),
+    (async () => {
+      let text = await fetchData("https://tenapi.cn/bilihot");
+      let list = destr(text)?.list;
+      if (list?.length) {
+        res.push(
+          ...list.map((item: any) => ({
+            title: item.showname,
+            url: item.url,
+            type: "哔哩哔哩",
+          }))
+        );
+      }
+    })(),
+    (async () => {
+      let text = await fetchData("https://tenapi.cn/zhihuresou");
+      let list = destr(text)?.list;
+      if (list?.length) {
+        res.push(
+          ...list.map((item: any) => ({
+            title: item.query,
+            url: item.url,
+            type: "知乎",
+          }))
+        );
+      }
+    })(),
+  ]);
+  return res;
 }, 1000 * 60 * 60 * 24);
 
-export default defineHandler(async () => {
-  return {
-    list: await fetchData(),
-  };
+export default defineHandler(async (e) => {
+  let list = await getTopSearch();
+  const { current, pageSize } = getQuery(e);
+  if (current) {
+    let page: Page = {
+      current: parseInt(`${current}`) || 1,
+      pageSize: parseInt(`${pageSize}`) || 10,
+    };
+    setTotal(page, list.length);
+    list = list.slice(
+      (page.current - 1) * page.pageSize,
+      page.current * page.pageSize
+    );
+    return {
+      list,
+      ...page,
+    };
+  } else {
+    return {
+      list,
+    };
+  }
 });
